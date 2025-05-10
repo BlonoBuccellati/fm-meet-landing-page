90cc2672ebdbe96df565b2477b9ef83e08b07937
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-05-10 16:09:38 +0900

    fix:iamgeの切り替えの追加

--- src/components/sections/footer-section.tsx
+++ src/components/sections/footer-section.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
-import { mobileImageFooter } from "@/assets";
+import {
+  mobileImageFooter,
+  tabletImageFooter,
+  desktopImageFooter,
+} from "@/assets";
 import Number from "@/components/number";
 
 import DownloadText from "../download-text";
@@ -11,12 +15,16 @@ const SectionHero = () => {
     <section className="desktop:text-left relative bg-cyan-600/85 text-center text-white">
       {/* fill で親のサイズにピッタリ合わせる */}
       {/* Imageは、デフォルトでabsolute */}
-      <Image
-        src={mobileImageFooter}
-        alt=""
-        fill
-        className="-z-10 object-cover object-center"
-      />
+      <picture>
+        <source media="(min-width:768px)" srcSet={tabletImageFooter.src} />
+        <source media="(min-width:1440px)" srcSet={desktopImageFooter.src} />
+        <Image
+          src={mobileImageFooter}
+          alt=""
+          fill
+          className="-z-10 object-cover object-center"
+        />
+      </picture>
       <div className="desktop:flex desktop:justify-around desktop:items-start desktop:m-0 desktop:px-1000 desktop:space-x-400 desktop:space-y-0 desktop:max-w-none mx-auto max-w-[31.5rem] space-y-300 px-400 py-1400">
         <h1 className="typo-heading-lg desktop:max-w-[22.1875rem]">
           Experience more together
